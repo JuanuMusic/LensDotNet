ef7391f9951ac941fc87ecb2f2a880dea9b43a11
Juanu <juanu@juanu.com.ar>
2023-06-13 18:06:48 -0300

    Blank spaces removal

--- src/Examples/LensDotNet.Examples.Console/ExploreProfiles.cs
+++ src/Examples/LensDotNet.Examples.Console/ExploreProfiles.cs
@@ -10,12 +10,9 @@ namespace LensDotNet.Examples.CLI
             var config = new LensConfig(LensConfig.DEVELOPMENT_GQL_ENDPOINT);
             var client = new LensClient(config);
             var profiles = await client.Explore.ExploreProfiles();
-
-            Console.WriteLine($"Found {profiles.Items.Length} profiles");
+            
             foreach (var profile in profiles.Items)
-            {
                 Console.WriteLine($"name: {profile.Name} - owner: {profile.OwnedBy} - id: {profile.Id}");
-            }
 
             Console.WriteLine("Explore Profiles completed.");
             Console.WriteLine(" Press any key to continue...");
